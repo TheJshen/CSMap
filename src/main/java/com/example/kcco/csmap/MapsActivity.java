bb7b1e408d1e4d2090139fd02d78b2a1fe084178
Jason Shen <Jshen1191@gmail.com>
2015-05-11 14:14:02 -0700

    First commit from Android Studio

---
+++ src/main/java/com/example/kcco/csmap/MapsActivity.java
@@ -0,0 +1,96 @@
+package com.example.kcco.csmap;
+
+import android.graphics.Color;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+//import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.model.VisibleRegion;
+
+//import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity {
+
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private static final LatLng UCSD = new LatLng(32.880114, -117.233981);
+    private static final LatLng GEISEL = new LatLng(32.881132, -117.237639);
+    private static CameraPosition cameraPosition;
+
+
+    private Polyline testLine;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Display map
+        setContentView(R.layout.activity_maps);
+        setUpMapIfNeeded();
+        // Create the test line from Center to Geisel
+        testLine = mMap.addPolyline(new PolylineOptions()
+                .add(UCSD, GEISEL)
+                .width(5)
+                .color(Color.RED));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p/>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        //mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(UCSD, 19));
+        //mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        // Sets the camera position to cameraPosition
+        cameraPosition = new CameraPosition.Builder()
+                .target(UCSD)      // Sets the center of the map to Mountain View
+                .zoom(19)                   // Sets the zoom
+                .bearing(0)                // Sets the orientation of the camera to North
+                .tilt(45)                   // Sets the tilt of the camera to 30 degrees
+                .build();                   // Creates a CameraPosition from the builder
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
+
+}
---
+++ src/main/java/com/example/kcco/csmap/Route.java
@@ -0,0 +1,21 @@
+package com.example.kcco.csmap;
+
+/**
+ * Created by jason on 5/6/15.
+ */
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.Date;
+
+public class Route {
+
+    private LatLng start, end;
+    private String createdBy;
+    private int routeTime;
+    Date createAt, updatedAt;
+
+    public Route() {
+
+    }
+}
