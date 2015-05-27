800037f5f87051d5e7e3bb5ec00999f953424d28
kachan2005 <sai90113@hotmail.com>
2015-05-26 17:24:39 -0700

    Create Package Misc for organized.

--- src/main/java/com/example/kcco/csmap/MapMainActivity.java
+++ src/main/java/com/example/kcco/csmap/MapMainActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.kcco.csmap.Misc.FlyOutContainer;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
