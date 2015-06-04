25079528a8dcc94f9ab6b60d037eddd986e824a5
kachan2005 <sai90113@hotmail.com>
2015-06-03 17:11:49 -0700

    Updated MapMainActivity, add function processDestination.

--- src/main/java/com/example/kcco/csmap/MapMainActivity.java
+++ src/main/java/com/example/kcco/csmap/MapMainActivity.java
@@ -78,6 +78,9 @@ public class MapMainActivity extends FragmentActivity implements RouteTracker.Lo
     private int selectedIndex = -1;
     private ArrayList<Pair<Polyline, Integer>> displayedLines = new ArrayList<>();
 
+    //variable
+    LatLng destinationLocation = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -193,6 +196,7 @@ public class MapMainActivity extends FragmentActivity implements RouteTracker.Lo
         clearRouteTrackingMarker();
         Route newRoute = new Route(route);
         Polyline newLine = mMap.addPolyline(newRoute.drawRoute());
+        processDestination();
         displayedLines.add(new Pair<>(newLine, routeId));
         isLinesDisplayed = true;
     }
@@ -209,6 +213,7 @@ public class MapMainActivity extends FragmentActivity implements RouteTracker.Lo
             displayedLines.add(new Pair<>(newLine, bestRoutes.get(index).second));
             isLinesDisplayed = true;
         }
+        processDestination();
     }
 
 
@@ -688,13 +693,11 @@ public class MapMainActivity extends FragmentActivity implements RouteTracker.Lo
 //                        Messenger.toast("TODO: I selected A route, added to history, need further functions", MapMainActivity.this);
 
 
-
                         //change selected route into red, save in history show addBookmark button
                         //change selected route into red
                         selectedIndex = thisSelectedIndex;
                         selectedRouteId = displayedLines.get(selectedIndex).second;
                         displayedLines.get(selectedIndex).first.setColor(Color.RED);
-
 
                     } else {
                         //hide Bookmark button, and reset any selected variables back to -1
@@ -723,18 +726,21 @@ public class MapMainActivity extends FragmentActivity implements RouteTracker.Lo
         }
     }
 
-    //TODO: tempory function script to trime all places name
-    private void trimPlaceName() throws InterruptedException {
-        for( int i = 0; i < 70; i++){
-            Thread.sleep(8000);
-            BuildingDAO thisBuilding = BuildingDAO.searchBuilding(i, MapMainActivity.this);
-            if( thisBuilding != null){
-                thisBuilding.setName(thisBuilding.getName().toLowerCase().trim());
-                thisBuilding.sendBuildingInfo();
-                Messenger.toast("Update building "+thisBuilding.getName()+", success", MapMainActivity.this);
-            }
-        }
+    public void processDestination(){
+
+        List<LatLng> points = displayedLines.get(0).first.getPoints();
+
+        double distance1 = RouteProcessing.getDistance(currentLocation, points.get(0));
+        double distance2 = RouteProcessing.getDistance(currentLocation, points.get(points.size()-1));
+
+        if( distance1 < distance2 )
+            destinationLocation = points.get(points.size()-1);
+        else
+            destinationLocation = points.get(0);
+
+
     }
+
 
 
 
